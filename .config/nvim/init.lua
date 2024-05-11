6cf0c78e66f8c80f0f781e8ce33272eaee4acceb
gyndlf <james@zingel.co.nz>
2024-05-11 18:31:54 +1000

    Updated git config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -180,6 +180,6 @@ vim.keymap.set('n', '<space>rh', '<cmd>IronHide<cr>')
 -- Activate the snippets (mainly for latex)
 -- Uses the "SnipMate-Like" method
 local ls = require("luasnip")
-vim.keymap.set({"i"}, "`", function() ls.expand() end, {silent = true})
+vim.keymap.set({"i"}, "<Tab>", function() ls.expand() end, {silent = true})
 require("luasnip.loaders.from_snipmate").lazy_load({paths = "./snippets"})
 
